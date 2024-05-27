4fde9b6aff770edacf854c0f88ea10e30354a8a5
Giulio Dille Girardi <giulio.girardi@edu.pucrs.br>
2024-05-26 21:56:05 -0300

    adiciona arquivo teste para conexão ssh

--- testeRun.sh
+++ testeRun.sh
@@ -2,23 +2,25 @@ numCli=12
 numServer=6
 duration=1
 locality=80
-rm -f -r logs/*  files/*; kill -l "java.*Main*"; ant clean; ant;
+#rm -f -r logs/*  files/*; kill -l "java.*Main*"; ant clean; ant;
 
 LOG_FILE="logs/teste.txt"
 HOST_MAIN="node0"
 echo $HOST_MAIN
 
 #Monta comando SSH
-SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
+SSH_COMMAND=" ssh -o StrictHostKeyChecking=no"
 
-COMMAND="screen -d -m -L -Logfile $LOG_FILE -S a0.$HOST_MAIN"
-COMMAND+=" $SSH_COMMAND "node1" java -cp "bin/*:lib/*" MainServer -i 1 -d $duration -c $numCli >> logs/node1.txt & sleep .05"
+COMMAND_1="screen -d -m -L -Logfile $LOG_FILE -S a0.$HOST_MAIN"
+COMMAND_1+="$SSH_COMMAND "node1" java -cp "bin/*:lib/*" MainServer -i 2 -d $duration -c $numCli"
+echo  COMMAND_1
 
-$COMMAND
-
+COMMAND_2="screen -d -m -L -Logfile $LOG_FILE -S a0.$HOST_MAIN"
+COMMAND_2+="$SSH_COMMAND "node1" java -cp "bin/*:lib/*" MainClient -c $numCli -i 1  -d $duration -t -l $locality -w 1"
+echo  COMMAND_2
 # Start clients
-java -cp "bin/*:lib/*" MainClient -c $numCli -i 0  -d $duration -t -l $locality -w 0 >> logs/cli0.txt &
-java -cp "bin/*:lib/*" MainClient -c $numCli -i 1  -d $duration -t -l $locality -w 1 >> logs/cli1.txt &
+#java -cp "bin/*:lib/*" MainClient -c $numCli -i 1  -d $duration -t -l $locality -w 1 >> logs/cli0.txt &
+#java -cp "bin/*:lib/*" MainClient -c $numCli -i 2  -d $duration -t -l $locality -w 2 >> logs/cli1.txt &
 
 echo started $numCli clients >> logs/executions.log
 
