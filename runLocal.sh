fc3152bbd4ba4c4546e763bdeebea97bb8a3e8af
Giulio Dille Girardi <giulio.girardi@edu.pucrs.br>
2024-06-03 20:48:44 -0300

    adiciona conexão ssh ao script runLocal

--- runLocal.sh
+++ runLocal.sh
@@ -1,13 +1,13 @@
-numCli=12
-numServer=6
-duration=120
+numCli=3
+numServer=3
+duration=60
 locality=80
 rm -f -r logs/*  files/*; pkill -f 'java.*Main*'; ant clean; ant;
 
 # Start servers
-((START = $numServer-1))
+((START = "${numServer}"-1))
 for ((i = START; i >= 0; i-=1)) ; do
-    java -cp "bin/*:lib/*" MainServer -i $i -d $duration -c $numCli >> logs/node$i.txt & sleep .05
+  sudo ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@node${i}" "$(java -cp "bin/*:lib/*" MainServer -i "${i}" -d $duration -c $numCli >> logs/node"${i}".txt & sleep .05)"
 done
 echo started $numServer servers >> logs/executions.log
 
@@ -15,15 +15,15 @@ echo started $numServer servers >> logs/executions.log
 java -cp "bin/*:lib/*" MainClient -c $numCli -i 0  -d $duration -t -l $locality -w 0 >> logs/cli0.txt &
 java -cp "bin/*:lib/*" MainClient -c $numCli -i 1  -d $duration -t -l $locality -w 1 >> logs/cli1.txt &
 java -cp "bin/*:lib/*" MainClient -c $numCli -i 2  -d $duration -t -l $locality -w 2 >> logs/cli2.txt &
-java -cp "bin/*:lib/*" MainClient -c $numCli -i 3  -d $duration -t -l $locality -w 3 >> logs/cli3.txt &
-java -cp "bin/*:lib/*" MainClient -c $numCli -i 4  -d $duration -t -l $locality -w 4 >> logs/cli4.txt &
-java -cp "bin/*:lib/*" MainClient -c $numCli -i 5  -d $duration -t -l $locality -w 5 >> logs/cli5.txt &
-java -cp "bin/*:lib/*" MainClient -c $numCli -i 6  -d $duration -t -l $locality -w 0 >> logs/cli6.txt &
-java -cp "bin/*:lib/*" MainClient -c $numCli -i 7  -d $duration -t -l $locality -w 1 >> logs/cli7.txt &
-java -cp "bin/*:lib/*" MainClient -c $numCli -i 8  -d $duration -t -l $locality -w 2 >> logs/cli8.txt &
-java -cp "bin/*:lib/*" MainClient -c $numCli -i 9  -d $duration -t -l $locality -w 3 >> logs/cli9.txt &
-java -cp "bin/*:lib/*" MainClient -c $numCli -i 10 -d $duration -t -l $locality -w 4 >> logs/cli10.txt &
-java -cp "bin/*:lib/*" MainClient -c $numCli -i 11 -d $duration -t -l $locality -w 5 >> logs/cli11.txt &
+#java -cp "bin/*:lib/*" MainClient -c $numCli -i 3  -d $duration -t -l $locality -w 3 >> logs/cli3.txt &
+#java -cp "bin/*:lib/*" MainClient -c $numCli -i 4  -d $duration -t -l $locality -w 4 >> logs/cli4.txt &
+#java -cp "bin/*:lib/*" MainClient -c $numCli -i 5  -d $duration -t -l $locality -w 5 >> logs/cli5.txt &
+#java -cp "bin/*:lib/*" MainClient -c $numCli -i 6  -d $duration -t -l $locality -w 0 >> logs/cli6.txt &
+#java -cp "bin/*:lib/*" MainClient -c $numCli -i 7  -d $duration -t -l $locality -w 1 >> logs/cli7.txt &
+#java -cp "bin/*:lib/*" MainClient -c $numCli -i 8  -d $duration -t -l $locality -w 2 >> logs/cli8.txt &
+#java -cp "bin/*:lib/*" MainClient -c $numCli -i 9  -d $duration -t -l $locality -w 3 >> logs/cli9.txt &
+#java -cp "bin/*:lib/*" MainClient -c $numCli -i 10 -d $duration -t -l $locality -w 4 >> logs/cli10.txt &
+#java -cp "bin/*:lib/*" MainClient -c $numCli -i 11 -d $duration -t -l $locality -w 5 >> logs/cli11.txt &
 
 echo started $numCli clients >> logs/executions.log
 
